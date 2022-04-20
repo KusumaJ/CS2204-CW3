9e5037497460c635911cc51a70a8a94becfd995a
KusumaJ <69179241+KusumaJ@users.noreply.github.com>
2022-04-20 14:48:54 +0700

    publish website trial

---
+++ js/movies.js
@@ -0,0 +1,64 @@
+function getMovies() {
+    return [
+        {
+            id:1,
+            type:"now",
+            thumbnail:"../img/thumbnail/wonders.jpg",
+            src:"wonders",
+            name:"Wonder",
+            cast:"Robert De Niro, Katharine Hepburn",
+            director:"Steven Spielberg",
+            duration: 130
+        },
+        {
+            id:2,
+            type:"now",
+            thumbnail:"../img/thumbnail/barbecue.jpg",
+            src:"barbecue",
+            name:"Barbecue",
+            cast:"Jack Nicholson, Meryl Streep",
+            director:"Martin Scorsese",
+            duration: 125
+        },
+        {
+            id:3,
+            type:"now",
+            thumbnail:"../img/thumbnail/nature.jpg",
+            src:"nature",
+            name:"Nature",
+            cast:"Marlon Brando, Elizabeth Taylor",
+            director:"Alfred Hitchcock",
+            duration: 113
+        },
+        {
+            id:4,
+            type:"upcoming",
+            thumbnail:"../img/thumbnail/casablanca.png",
+            src:"casablanca-s",
+            name:"Casablanca",
+            cast:"Denzel Washington, Ingrid Bergman",
+            director:"James Cameron",
+            duration: 128
+        },
+        {
+            id:5,
+            type:"upcoming",
+            thumbnail:"../img/thumbnail/wildlife.jpg",
+            src:"wildlife",
+            name:"Wildlife",
+            cast:"Humphrey Bogart, Bette Davis",
+            director:"Spike Lee",
+            duration: 130
+        },
+        {
+            id:6,
+            type:"upcoming",
+            thumbnail:"../img/thumbnail/chocolate.jpg",
+            src:"chocolate",
+            name:"Chocolate",
+            cast:"Daniel Day-Lewis, Cate Blanchett",
+            director:"Quentin Tarantino",
+            duration: 137
+        }
+    ]
+}
